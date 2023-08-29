c6419267fe5592f61fb0f2820d9b6c96c608e6aa
singuneet <33375775+singuneet@users.noreply.github.com>
2023-08-29 19:03:23 +0530

    Update hx_ed.c
    1. Added Initialization for flag: The flag variable is now initialized to 1 by default at the beginning of the program. This ensures that if no conversion mode is specified, the program will default to binary-to-hexadecimal conversion.
    2. File Opening Mode ("rb"): When opening the input file using fopen, the mode "rb" is now used, indicating that the file should be opened in binary mode for reading.
    3. Enhanced Error Handling for No Input File: If no input file is provided in the command-line arguments, the program now calls the error_no_input_file() function to display an appropriate error message.
    4. Improved Function Call Comments: Comments have been added near function calls to indicate what each function is expected to do. This helps in better understanding the purpose of each function call.

--- hx_ed.c
+++ hx_ed.c
@@ -4,43 +4,51 @@
 
 int main(int argc, char** argv)
 {
-    int flag=1;
+    int flag = 1;
 
     FILE* in_file = NULL;
 
-    for(int i = 1; i < argc; i++)
+    for (int i = 1; i < argc; i++)
     {
-	if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0)
-	{
-	    print_help(argv[0]);
-	    return 0;
-	}
-	else if (strcmp(argv[i], "-b2h") == 0 || strcmp(argv[i], "--binary_to_hex") == 0)
-	{
-	    flag = 1;    
-	}
-	else if(strcmp(argv[i], "-h2b") == 0 || strcmp(argv[i], "--hex_to_binary") == 0)
-	{
-	    flag = 0;
-	}
-	else
-	{
-	    if (in_file != NULL)
-	    {
-		error_infile(argv[0]);
-		return 1;
-	    }
-	    in_file = fopen(argv[i], "r");
-	    if (in_file == NULL)
-	    {
-		error_infile_open();
-		return 2;
-	    }
-	}
+        if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0)
+        {
+            print_help(argv[0]);
+            return 0;
+        }
+        else if (strcmp(argv[i], "-b2h") == 0 || strcmp(argv[i], "--binary_to_hex") == 0)
+        {
+            flag = 1;
+        }
+        else if (strcmp(argv[i], "-h2b") == 0 || strcmp(argv[i], "--hex_to_binary") == 0)
+        {
+            flag = 0;
+        }
+        else
+        {
+            if (in_file != NULL)
+            {
+                error_infile(argv[i]);
+                return 1;
+            }
+            in_file = fopen(argv[i], "rb");
+            if (in_file == NULL)
+            {
+                error_infile_open();
+                return 2;
+            }
+        }
     }
+
+    if (in_file == NULL)
+    {
+        error_no_input_file();
+        return 3;
+    }
+
     if (flag)
-	bin2hex(in_file, stdout);
-    else hex2bin(in_file, stdout);
+        bin2hex(in_file, stdout);
+    else
+        hex2bin(in_file, stdout);
 
     fclose(in_file);
     return 0;
