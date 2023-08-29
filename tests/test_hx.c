b9d76c6c15caa3380f15529745734522e6d80216
waseemR02 <waseem.riaz.9999@gmail.com>
2023-08-29 17:00:43 +0530

    Refactor tests

--- tests/test_hx.c
+++ tests/test_hx.c
@@ -33,32 +33,40 @@ int compareFiles(FILE* fp1, FILE* fp2) {
     }
 }
 
-void test_hx(void) {
+void test_bin2hex(void) {
   FILE* hexdump = fopen("hexdump.txt", "w+");
-  FILE* binary = fopen("binary.txt", "w+");
   FILE* test_hex2bin = fopen("test_hex2bin.txt", "r");
   FILE* test_bin2hex = fopen("test_bin2hex.txt", "r");
 
   bin2hex(test_hex2bin, hexdump);
-  hex2bin(test_bin2hex, binary);
   TEST_ASSERT_EQUAL(1, compareFiles(hexdump, test_bin2hex));
-  TEST_ASSERT_EQUAL(1, compareFiles(binary, test_hex2bin));
-  //TEST_ASSERT_EQUAL(1, compareFiles(fopen("test1.txt", "r"), fopen("test2.txt", "r")));
 
+  fclose(test_hex2bin);
+  fclose(test_bin2hex);
   fclose(hexdump);
+  remove("hexdump.txt");
+}
+
+void test_hex2bin(void) {
+  FILE* binary = fopen("binary.txt", "w+");
+  FILE* test_bin2hex = fopen("test_bin2hex.txt", "r");
+  FILE* test_hex2bin = fopen("test_hex2bin.txt", "r");
+
+  hex2bin(test_bin2hex, binary);
+  TEST_ASSERT_EQUAL(1, compareFiles(binary, test_hex2bin));
+
   fclose(binary);
   fclose(test_hex2bin);
   fclose(test_bin2hex);
-  remove("./hexdump.txt");
-  remove("./binary.txt");
+  remove("binary.txt");
 }
-
 void setUp() {}
 void tearDown() {}
 
 int main(void)
 {
 UNITY_BEGIN();
-RUN_TEST(test_hx);
+RUN_TEST(test_hex2bin);
+RUN_TEST(test_bin2hex);
 return UNITY_END();
 }
